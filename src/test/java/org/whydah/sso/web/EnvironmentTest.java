4d442d24a688ac35fc1de521f78e79abae8ad61a
Totto <totto@totto.org>
2014-08-29 13:10:14 +0200

    Cleanup

--- src/test/java/org/whydah/sso/web/EnvironmentTest.java
+++ src/test/java/org/whydah/sso/web/EnvironmentTest.java
@@ -2,13 +2,6 @@ package org.whydah.sso.web;
 
 import junit.framework.TestCase;
 
-/**
- * Created by IntelliJ IDEA.
- * User: totto
- * Date: 2/7/11
- * Time: 8:40 AM
- * To change this template use File | Settings | File Templates.
- */
 public class EnvironmentTest  extends TestCase {
 
     public void testGetHost() {
