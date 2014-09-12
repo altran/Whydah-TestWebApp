1fe85fa7d6831db847f02a6a5d40f805642ef758
Totto <totto@totto.org>
2014-09-12 20:55:23 +0200

    cookie confusion :)

--- src/main/java/org/whydah/sso/web/LoginController.java
+++ src/main/java/org/whydah/sso/web/LoginController.java
@@ -27,7 +27,7 @@ import java.net.UnknownHostException;
 @Controller
 public class LoginController {
 	private static final Logger log = LoggerFactory.getLogger(LoginController.class);
-    static final String USER_TOKEN_REFERENCE_NAME = "whydahusertoken_sso";
+    static final String USER_TOKEN_REFERENCE_NAME = "test_whydahusertoken";
 
     static String REDIRECT_TO_LOGIN_SERVICE; //"redirect:http://"+getHost()+":" + SSO_PORT + "/ssoHelper/login" + "?redirectURI=http://"+getHost()+":" + MY_PORT + "/test/hello";
     static String REDIRECT_TO_LOGOUT_SERVICE; // = "redirect:http://"+getHost()+":" + SSO_PORT + "/ssoHelper/logout" + "?redirectURI=http://"+getHost()+":" + MY_PORT + "/test/hello";
